1c56cd70619dd37c5976baa0b34f2be91f88a39f
Paul Dhanda <pauldhanda@live.com>
2020-05-31 21:24:07 -0700

    final commit

--- a1/generate_data_set.py
+++ a1/generate_data_set.py
@@ -3,15 +3,56 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path  # python3 only
 import spacy
+import csv
+import re
 
 def main():
     load_crendentials()
     api = twitter_login()
-    tweets = generate_tweets(api)
-    tokens = tokenize(tweets)
-    print (tokens)
 
-def top_tokens(n. tokens):
+    # generate covid-19 tweets
+    tweets = generate_tweets("#covid19",api)
+    tokens = tokenize(tweets)
+    print(len(tokens))
+    top_100 = top_tokens(100, tokens)
+    to_csv_freq("covid-top100.csv",top_100)
+    to_csv_all("covid.csv",tokens)
+
+    # generate normal tweets
+    tweets = generate_tweets("*",api)
+    tokens = tokenize(tweets)
+    print(len(tokens))
+    top_100 = top_tokens(100, tokens)
+    to_csv_freq("normal-top100.csv",top_100)
+    to_csv_all("normal.csv",tokens)
+
+def to_csv_all(filename, tokens):
+    csv_columns = ['Weight','Word']
+    try:
+        with open(filename, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(csv_columns)
+            for key, value in tokens.items():
+                writer.writerow([value, key])
+
+    except IOError:
+        print("I/O error")
+
+def to_csv_freq(filename, tokens):
+    csv_columns = ['Frequency','Word']
+    try:
+        with open(filename, 'w') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(csv_columns)
+            for key, value in tokens.items():
+                writer.writerow([value/1000, key])
+
+    except IOError:
+        print("I/O error")
+
+def top_tokens(n, tokens):
+    sorted_tokens = {k: v for k, v in sorted(tokens.items(), key=lambda item: item[1], reverse=True)[:n]}
+    return sorted_tokens
 
 def tokenize(tweets):
     tokens = {}
@@ -22,17 +63,17 @@ def tokenize(tweets):
         doc = nlp(tweet)
         for token in doc:
             text = token.text.lower()
-            if text not in all_stopwords and not token.is_space and not token.is_punct:
+            if (text not in all_stopwords and re.match("^[A-Za-z0-9_-]*$", text)):
                 if text in tokens:
                     tokens[text] += 1
                 else:
                     tokens[text] = 1
     return tokens
 
-def generate_tweets(api):
+def generate_tweets(keyword,api):
     N = 1000
     tweets = []
-    for tweet_info in tweepy.Cursor(api.search, q='#covid-19', lang = 'en',  tweet_mode='extended').items(N):
+    for tweet_info in tweepy.Cursor(api.search, q=keyword, lang = 'en',  tweet_mode='extended').items(N):
         if "retweeted_status" in dir(tweet_info):
             tweet=tweet_info.retweeted_status.full_text
         else:
